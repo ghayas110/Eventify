972459fa95600e35d3871c368aad915e0920babe
sidra027 <72914089+sidra027@users.noreply.github.com>
2020-10-15 15:24:52 +0500

    changes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hamza Bilal Zafar</Text>
+      <Text>sidra ghayas hamza zafar</Text>
       <StatusBar style="auto" />
     </View>
   );
