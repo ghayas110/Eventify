599d83cd66fe5dfbfcd9fd1b677ea22e74e5879b
sidra027 <72914089+sidra027@users.noreply.github.com>
2020-10-17 21:04:40 +0500

    changes

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/Homescreen'
+const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
